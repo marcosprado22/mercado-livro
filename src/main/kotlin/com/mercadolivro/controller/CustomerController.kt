28106976788b69c110f71db0d6492003a6e4a997
Marcos Prado <marcospradocwb22@gmail.com>
2025-03-11 20:51:08 -0300

    initial commit

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,40 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/customers")
+class CustomerController {
+
+    val customers = mutableListOf<CustomerModel>()
+
+    @GetMapping
+    fun getAll(): List<CustomerModel> {
+        return customers
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createCustomer(@RequestBody customer: PostCustomerRequest) {
+
+        val id = if(customers.isEmpty()){
+            1
+        } else {
+            customers.last().id.toInt() + 1
+        }.toString()
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
+        println(customer)
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.filter { it.id == id }.first()
+    }
+
+}
+
